b51918a9652de46433a9587204dde9ff79c79ea7
Winston Spencer Sonnesen <sonnesen@gmail.com>
2017-06-27 01:35:29 -0300

    5.Bloqueando o cliente

--- src/main/java/com/totvs/cliente/Cliente.java
+++ src/main/java/com/totvs/cliente/Cliente.java
@@ -3,12 +3,18 @@ package com.totvs.cliente;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class Cliente {
 
 	public static void main(String[] args) throws UnknownHostException, IOException {
 		Socket socket = new Socket("localhost", 55555);
 		System.out.println("Cliente Conectado");
+		
+		Scanner scanner = new Scanner(System.in);
+		scanner.nextLine();
+		
+		scanner.close();
 		socket.close();
 
 	}
