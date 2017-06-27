27433f5c15651a7ac5ab4fe64ef64556de583d5e
Winston Spencer Sonnesen <sonnesen@gmail.com>
2017-06-27 01:30:25 -0300

    4.Simulando demora no servidor

--- src/main/java/com/totvs/servidor/Servidor.java
+++ src/main/java/com/totvs/servidor/Servidor.java
@@ -6,13 +6,14 @@ import java.net.Socket;
 
 public class Servidor {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, InterruptedException {
 		System.out.println("Iniciando servidor");
 		ServerSocket server = new ServerSocket(55555);
 		
 		while(true) {
 			Socket socket = server.accept();
 			System.out.printf("Novo cliente conectado na porta: %d\n", socket.getPort());
+			Thread.sleep(10000);
 		}
 	}
 
