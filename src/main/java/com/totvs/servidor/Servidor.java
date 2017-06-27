b2c7f681157ed339b040da36941d17f87822da0f
Winston Spencer Sonnesen <sonnesen@gmail.com>
2017-06-27 02:33:11 -0300

    15.Refatorando o servidor

--- src/main/java/com/totvs/servidor/Servidor.java
+++ src/main/java/com/totvs/servidor/Servidor.java
@@ -8,16 +8,34 @@ import java.util.concurrent.Executors;
 
 public class Servidor {
 
-	public static void main(String[] args) throws IOException, InterruptedException {
+	private boolean isRunning;
+	private ExecutorService threadPool;
+	private ServerSocket server;
+	
+	public Servidor() throws IOException {
 		System.out.println("Iniciando servidor");
-		ServerSocket server = new ServerSocket(55555);
-		ExecutorService threadPool = Executors.newCachedThreadPool();
-		
-		while(true) {
+		server = new ServerSocket(55555);
+		threadPool = Executors.newCachedThreadPool();
+		isRunning = true;
+	}
+	
+	public void run() throws IOException {
+		while(isRunning) {
 			Socket socket = server.accept();
 			System.out.printf("Novo cliente conectado na porta: %d\n", socket.getPort());
 			threadPool.execute(new TarefaCliente(socket));
-		}
+		}	
+	}
+	
+	public void stop() throws IOException {
+		isRunning = false;
+		threadPool.shutdown();
+		server.close();
+	}
+	
+	public static void main(String[] args) throws IOException, InterruptedException {
+		Servidor servidor = new Servidor();
+		servidor.run();
 	}
 
 }
