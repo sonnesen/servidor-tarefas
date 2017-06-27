cfa8a3b7c748ddcbf9d3b966ac22bfdfc6392d2a
Winston Spencer Sonnesen <sonnesen@gmail.com>
2017-06-27 01:26:59 -0300

    3.Aceitando varios cliente

--- src/main/java/com/totvs/servidor/Servidor.java
+++ src/main/java/com/totvs/servidor/Servidor.java
@@ -2,13 +2,18 @@ package com.totvs.servidor;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Servidor {
 
 	public static void main(String[] args) throws IOException {
 		System.out.println("Iniciando servidor");
 		ServerSocket server = new ServerSocket(55555);
-		server.accept();
+		
+		while(true) {
+			Socket socket = server.accept();
+			System.out.printf("Novo cliente conectado na porta: %d\n", socket.getPort());
+		}
 	}
 
 }
