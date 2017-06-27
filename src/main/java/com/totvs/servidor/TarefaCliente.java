4115f45df894c00c799cee5d942a7188a6d186f5
Winston Spencer Sonnesen <sonnesen@gmail.com>
2017-06-27 02:25:55 -0300

    14.Confirmando comando recebido

--- src/main/java/com/totvs/servidor/TarefaCliente.java
+++ src/main/java/com/totvs/servidor/TarefaCliente.java
@@ -1,6 +1,7 @@
 package com.totvs.servidor;
 
 import java.io.IOException;
+import java.io.PrintStream;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -14,11 +15,24 @@ public class TarefaCliente implements Runnable {
 	
 	@Override
 	public void run() {
-		try (Scanner scanner = new Scanner(socket.getInputStream())) {
+		try (Scanner scanner = new Scanner(socket.getInputStream());
+				PrintStream printStream = new PrintStream(socket.getOutputStream())) {
 			System.out.println("Executando tarefa do cliente " + socket.getPort());
 			
 			while (scanner.hasNextLine()) {
-				System.out.println(scanner.nextLine());
+				String nextLine = scanner.nextLine();
+				System.out.println("Comando recebido: " + nextLine);
+				
+				switch (nextLine) {
+				case "c1":
+					printStream.println("Comando 1 recebido");
+					break;
+				case "c2":
+					printStream.println("Comando 2 recebido");
+					break;
+				default:
+					printStream.println("Comando desconhecido");
+				}
 			}
 			
 		} catch (IOException e) {
