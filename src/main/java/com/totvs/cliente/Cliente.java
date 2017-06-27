6693cee4ec8880e3565ccca01006470ac136e638
Winston Spencer Sonnesen <sonnesen@gmail.com>
2017-06-27 02:16:22 -0300

    12.Criando threads para enviar e receber dados do servidor no cliente

--- src/main/java/com/totvs/cliente/Cliente.java
+++ src/main/java/com/totvs/cliente/Cliente.java
@@ -11,25 +11,42 @@ public class Cliente {
 	public static void main(String[] args) throws UnknownHostException, IOException {
 		Socket socket = new Socket("localhost", 55555);
 		System.out.println("Cliente Conectado");
-		
-		PrintStream printStream = new PrintStream(socket.getOutputStream());
-		
-		Scanner scanner = new Scanner(System.in);
-		while(scanner.hasNextLine()) {			
-			String nextLine = scanner.nextLine();
-			if (("").equals(nextLine)) break;
-			printStream.println(nextLine);
-		}
-		
-		System.out.println("Recebendo dados do servidor");
-		Scanner scanner2 = new Scanner(socket.getInputStream());
-		while (scanner2.hasNextLine()) {
-			System.out.println(scanner2.nextLine());
-		}
-		scanner2.close();
-		
-		printStream.close();
-		scanner.close();
+
+		// enviando dados para o servidor
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				System.out.println("Enviando dados para o servidor");
+				try (PrintStream printStream = new PrintStream(socket.getOutputStream());
+						Scanner scanner = new Scanner(System.in)) {
+
+					while (scanner.hasNextLine()) {
+						String nextLine = scanner.nextLine();
+						if (("").equals(nextLine))
+							break;
+						printStream.println(nextLine);
+					}
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+			}
+		}).start();
+
+		// recebendo dados do servidor
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				System.out.println("Recebendo dados do servidor");
+				try (Scanner scanner2 = new Scanner(socket.getInputStream())) {
+					while (scanner2.hasNextLine()) {
+						System.out.println(scanner2.nextLine());
+					}
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+			}
+		}).start();
+
 		socket.close();
 
 	}
